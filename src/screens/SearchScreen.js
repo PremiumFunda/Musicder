e1d88d52da0850a134d30072dd1317750b295a31
PremiumFunda <54646127+PremiumFunda@users.noreply.github.com>
2021-03-24 20:25:27 +0500

    Update SearchScreen.js

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -57,14 +57,7 @@ export default class SearchScreen extends React.Component {
                         </div>
                     ))}
 
-                    <div className="footerdiv">
-                        <img alt="Musicder" className="center" src="./img/logo.png" width="230" height="92" />
-                        <a className="atextdec" href={`https://github.com/cachecleanerjeet`}>
-                            <p className="paragone" >An Open Source Project by Tuhin</p>
-                        </a><br />
-                        <a href={`https://github.com/cachecleanerjeet/Musicder`}>
-                            <img alt="Github" className="center" src="./img/github-white.svg" width="30" height="30" /><br />
-                        </a>
+                    
                     </div>
                 </div>
             )
